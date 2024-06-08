c13c3e1428eea30c05f3efc912ad9e4fcdf331c7
shanick <shanlogauthier@gmail.com>
2024-06-08 15:34:33 +0200

    Add button to enable win popup

--- Assets/Business/ColorButton/ColorButtonView.cs
+++ Assets/Business/ColorButton/ColorButtonView.cs
@@ -1,21 +1,36 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Business.ColorButton
 {
     public class ColorButtonView : MonoBehaviour
     {
+        [SerializeField] private WinPopup winPopup;
+        
         public Button _button;
+        
         private void Awake()
         {
             _button = GetComponent<Button>();
             _button.onClick.AddListener(OnColorButtonClicked);
+            winPopup.AnimationFinished += OnAnimationFinished;
+        }
+
+        private void OnAnimationFinished()
+        {
+            winPopup.Hide();
         }
 
         private void OnColorButtonClicked()
         {
-            Debug.Log("El boton fue clickeado");
+            winPopup.Show();
+        }
+
+        private void OnDisable()
+        {
+            winPopup.AnimationFinished -= OnAnimationFinished;
         }
     }
 }
---
+++ Assets/Business/WinPopup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Business
+{
+    public class WinPopup : MonoBehaviour
+    {
+        public event Action AnimationFinished;
+
+        private void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            StartCoroutine(ChangeColorCoroutine());
+        }
+
+        private IEnumerator ChangeColorCoroutine()
+        {
+            yield return new WaitForSeconds(3);
+            GetComponent<Image>().color = Color.green;
+            yield return new WaitForSeconds(3);
+            AnimationFinished?.Invoke();
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            AnimationFinished = null;
+        }
+    }
+}
